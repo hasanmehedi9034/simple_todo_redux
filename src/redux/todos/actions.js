fae9bfa172d912b3bb566a91d254664f50d5fe89
hasanmehedi9034 <mehedi90344@gmail.com>
2023-03-27 16:00:05 +0600

    actions and actionsType created

---
+++ src/redux/todos/actions.js
@@ -0,0 +1,48 @@
+import { ADDED, ALLCOMPLTED, CLEARCOMPLETED, COLORSELECTED, DELETED, TOGGLED } from "./actionTypes"
+
+export const added = (todoText) => {
+    return {
+        type: ADDED,
+        payload: todoText
+    }
+}
+
+export const toggled = (todoId) => {
+    return {
+        type: TOGGLED,
+        payload: todoId
+    }
+}
+
+export const colorSelected = (todoId, color) => {
+    return {
+        type: COLORSELECTED,
+        payload: {
+            todoId,
+            color
+        }
+    }
+}
+
+export const deleted = (todoId) => {
+    return {
+        type: DELETED,
+        payload: todoId
+    }
+}
+
+export const allCompleted = () => {
+    return {
+        type: ALLCOMPLTED,
+    }
+}
+
+export const clearCompleted = () => {
+    return {
+        type: CLEARCOMPLETED,
+    }
+}
+
+
+
+
