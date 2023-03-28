13f9931c606863c97fd01060317b20e5e993c350
hasanmehedi9034 <mehedi90344@gmail.com>
2023-03-28 13:20:43 +0600

    filter section completed

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,10 +3,28 @@ import Todo from "./Todo";
 
 export default function TodoList() {
     const todos = useSelector(state => state.todos);
+    const {status, colors} = useSelector(state => state.filters);
     
     return (
         <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-            {todos.map(todo => <Todo key={todo.id} todo={todo} />)}
+            {todos
+            .filter(todo => {
+                switch(status) {
+                    case 'Complete':
+                        return todo.completed === true
+                    case 'Incomplete':
+                        return todo.completed === false
+                    default:
+                        return true
+                }
+            })
+            .filter(todo => {
+                if(colors.length > 0) {
+                    return colors.includes(todo?.color)
+                }
+                return true
+            })
+            .map(todo => <Todo key={todo.id} todo={todo} />)}
         </div>
     );
 }
